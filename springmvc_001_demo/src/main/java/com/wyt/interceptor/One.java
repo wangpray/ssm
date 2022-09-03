88bc788dd3ac8e189ee0a247784175b564ac14eb
wangpray <354178724@qq.com>
2022-09-03 18:58:51 +0800

    新增加的一个类One

---
+++ springmvc_001_demo/src/main/java/com/wyt/interceptor/One.java
@@ -0,0 +1,9 @@
+package com.wyt.interceptor;
+
+import java.io.OutputStreamWriter;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println("新增加的一个类");
+    }
+}
